9c33862568ad1833911d9d0016a9be7b82e7bfde
Laura <105425865+Cyber-laura@users.noreply.github.com>
2023-02-02 10:59:41 +0100

    MODIF-CARD

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -30,23 +30,25 @@ function Card() {
             </section>
 
             <div className="Name-City-Note-Flag-Icone">
-                <div className="info-logement">
-                    <h1>{cardId.title}</h1>
-                    <h2>{cardId.location}</h2>
-                    <div className="tag">
-                        {cardId.tags.map((element, index) => {
-                            return (<span key={index}>{element}</span>)
-                        })
-                        }
+                <section className="section-info-hôte">
+                    <div className="info-logement">
+                        <h1>{cardId.title}</h1>
+                        <h2>{cardId.location}</h2>
+                        <div className="tag">
+                            {cardId.tags.map((element, index) => {
+                                return (<span key={index}>{element}</span>)
+                            })
+                            }
+                        </div>
                     </div>
-                </div>
-                <div className="propriétaire-rate">
-                    <div className="information-prorietaire">
-                        <span className="nom-du proprio">{cardId.host.name}</span>
-                        <img src={cardId.host.picture} alt="hôte" />
-                    </div>
+                    <div className="propriétaire-rate">
+                        <div className="information-prorietaire">
+                            <span className="nom-du proprio">{cardId.host.name}</span>
+                            <img src={cardId.host.picture} alt="hôte" />
+                        </div>
 
-                </div>
+                    </div>
+                </section>
                 <div className="equipement-description">
                     <span onClick={handleDropdown}>Description</span>
                     {bool && <><div className="description">
